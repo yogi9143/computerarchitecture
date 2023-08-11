319fd9ab0317bde9ebad53bf6ea77caa1d00a37f
yogi9143 <112718072+yogi9143@users.noreply.github.com>
2023-08-11 13:17:15 +0530

    Add files via upload

---
+++ EXP 38.cpp
@@ -0,0 +1,47 @@
+counter=1
+a=int(input("ENTER NUMBER-1-"))
+counter=counter+1
+b=int(input("ENTER NUMBER-2-"))
+counter=counter+1
+res= a and b
+counter=counter+1
+print(res)
+counter=counter+2
+INS=int(input("enter no. of instructions:"))
+performance_measure=INS/counter
+print("performance measure:",performance_measure)
+ 
+counter=1
+a=int(input("ENTER NUMBER-1-"))
+counter=counter+1
+b=int(input("ENTER NUMBER-2-"))
+counter=counter+1
+print("1-ADDITION 2-SUBTRACTION 3-MULTIPLICATION 4-DIVISION")
+print("Enter Your Choice")
+choice=int(input())
+if choice==1:
+ print("Performing Addition...")
+ res=a+b
+ counter=counter+1
+if choice==2:
+ print("Performing Subtraction...")
+ res=a-b
+ counter=counter+1
+if choice==3:
+ print("Performing Multiplication")
+ res=a*b
+ counter=counter+1
+if choice==4:
+ if b==0:
+ print("Denominator can't be Zero")
+ print("Performing Division")
+ res=a/b
+ counter=counter+1
+if choice>=5:
+ print("Enter Correct Input")
+print(res)
+counter=counter+3
+print("CYCLE VALUE IS:",counter)
+ins=int(input("Enter the No.of instructions:"))
+performance_measure =ins/counter
+print("performance measure is:",performance_measure)
